f0a30a10e02b47daaf62072037776a2d3bd701d2
Tiago Pereira <tap.bnu@gmail.com>
2026-03-05 14:16:19 -0300

    melhoria score v3 usando PESO_FREQUENCIA

--- core/statistics/score_calculator.py
+++ core/statistics/score_calculator.py
@@ -8,6 +8,9 @@ class ScoreCalculator:
 
     @staticmethod
     def score_frequencia(jogo, mapa_frequencia, max_freq):
+        """
+        Calcula score baseado na frequência histórica das dezenas.
+        """
         soma_freq = sum(mapa_frequencia.get(d, 0) for d in jogo.dezenas)
         media_freq = soma_freq / len(jogo.dezenas)
 
@@ -18,16 +21,25 @@ class ScoreCalculator:
 
     @staticmethod
     def score_soma(jogo):
+        """
+        Score gaussiano baseado na soma das dezenas.
+        """
         diferenca = jogo.soma - ScoreConfig.MEDIA_SOMA
         return math.exp(-(diferenca ** 2) / (2 * (ScoreConfig.DESVIO_SOMA ** 2)))
 
     @staticmethod
     def score_pares(jogo):
+        """
+        Score gaussiano baseado na quantidade de pares.
+        """
         diferenca = jogo.pares - ScoreConfig.MEDIA_PARES
         return math.exp(-(diferenca ** 2) / (2 * (ScoreConfig.DESVIO_PARES ** 2)))
 
     @staticmethod
     def score_atraso(jogo, mapa_atraso, max_atraso):
+        """
+        Score baseado no atraso das dezenas.
+        """
         if max_atraso == 0:
             return 0.0
 
@@ -37,8 +49,17 @@ class ScoreCalculator:
         return media_atraso / max_atraso
 
     @classmethod
-    def calcular_score(cls, jogo, mapa_frequencia, max_freq,
-                       mapa_atraso=None, max_atraso=1):
+    def calcular_score(
+        cls,
+        jogo,
+        mapa_frequencia,
+        max_freq,
+        mapa_atraso=None,
+        max_atraso=1
+    ):
+        """
+        Calcula score final ponderado do jogo.
+        """
 
         sf = cls.score_frequencia(jogo, mapa_frequencia, max_freq)
         ss = cls.score_soma(jogo)
@@ -49,7 +70,7 @@ class ScoreCalculator:
             sa = cls.score_atraso(jogo, mapa_atraso, max_atraso)
 
         score_final = (
-            sf +
+            ScoreConfig.PESO_FREQUENCIA * sf +
             ScoreConfig.PESO_SOMA * ss +
             ScoreConfig.PESO_PARES * sp +
             ScoreConfig.PESO_ATRASO * sa
