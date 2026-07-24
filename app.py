65c1671d239f173853ebce53a9b8376f6f06e008
Tiagoblu <tap.bnu@gmail.com>
2026-07-24 13:54:52 -0300

    criando arquivo inicial do streamlit

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Lotofácil Pro V11", page_icon="🎯")
+
+st.title("🎯 Lotofácil Pro V11")
+st.subheader("Sistema de Análise Preditiva")
+
+st.info("💡 A interface web está sendo configurada com sucesso!")
